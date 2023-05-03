99b3050508cbd8fff4f7971b1cde842b87d3ede1
CJDS7 <cjds2000@gmail.com>
2023-05-02 21:39:38 -0300

    Exercicio arrays 02.05

--- index.js
+++ index.js
@@ -83,11 +83,110 @@
 // console.log(dog)
 
 
-const numeros = [1, 2, 3, 4, 5, 6];
-console.log(numeros.length);
-numeros.push(7)
-console.log(numeros)
-numeros.splice(3,2)
-console.log(numeros)
-console.log(numeros.length);
+// const numeros = [1, 2, 3, 4, 5, 6];
+// console.log(numeros.length);
+// numeros.push(7)
+// console.log(numeros)
+// numeros.splice(3,2)
+// console.log(numeros)
+// console.log(numeros.length);
+
+//---------------------------------------------------------------------------------------------
+
+//02/05
+
+
+//Interpretação de código
+
+//1
+
+
+// let array
+// console.log('a. ', array) //undefined
+
+// array = null
+// console.log('b. ', array) //null
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)  //11
+
+// let i = 0
+// console.log('d. ', array[i])  //3
+
+// array[i+1] = 19
+// console.log('e. ', array)  // Array(11)
+//                               0: 3
+//                               1: 19
+//                               2: 5
+//                               3: 6
+//                               4: 7
+//                               5: 8
+//                               6: 9
+//                               7: 10
+//                               8: 11
+//                               9: 12
+//                              10: 13
+//                          length: 11
+//                 [[Prototype]]: Array(0)
+
+
+// const valor = array[i+6]
+// console.log('f. ', valor) //9
+
+// //2
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//Qual será o valor impresso no console se a entrada do usuário for: "Subi num ônibus em Marrocos"?
+
+//toUpperCas - converte para maiusculo
+//length - conta letras e espaços
+//replaceAll - converte as palavras ou letras solicitadas
+
+
+//Exercicios de escrita
+
+//1
+
+// const emailDoUsuario = prompt ("digite seu email")
+// const nomeDoUsuario = prompt ("digite seu nome")
+// console.log (`O e-mail  ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`);
+
+
+//2
+
+// const comidaFavorita = ["sushi", "pizza", "churrasco", "chocolate", "melancia"]; 
+// console.log(comidaFavorita) 
+// console.log (`Essas são as minhas comidas preferidas: ${comidaFavorita}`)
+
+// const comidaFavoritaUsuario = prompt ("digite uma comida favorita")
+// console.log(comidaFavoritaUsuario)
+
+// comidaFavorita[1] = comidaFavoritaUsuario
+// console.log(comidaFavorita)
+
+//3
+
+// const listaDeTarefas = []; 
+// console.log(listaDeTarefas)
+
+// const Tarefa1 = prompt ("1 tarefa que precisa realizar no seu dia");
+
+// const Tarefa2 = prompt ("2 tarefa que precisa realizar no seu dia");
+
+// const Tarefa3 = prompt ("3 tarefa que precisa realizar no seu dia");
+
+// listaDeTarefas.push(Tarefa1, Tarefa2, Tarefa3)
+// console.log(listaDeTarefas)
+// const tarefaRealizada = Number(prompt("digite o índice de uma tarefa que já realizou: 1, 2 ou 3"))
+// console.log(tarefaRealizada)
+
+// listaDeTarefas.splice(tarefaRealizada -1,1)
+// console.log(listaDeTarefas)
+
+
+const frase = prompt("Digite uma frase")
+console.log("frase:, frase);
+console.log("frase array: "),frase.split(" "));
 
